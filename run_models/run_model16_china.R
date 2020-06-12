77d6e3044defa9cb7210386f9af12f74df021ab7
InazeaAnazasi <inazea@posteo.org>
2020-06-12 14:25:32 +0200

    corrected some overfull lines in run_mode16_china.R

--- run_models/run_model16_china.R
+++ run_models/run_model16_china.R
@@ -63,7 +63,33 @@ qsum(kappa)
 
 ## Contact matrix in China ----
 
-contact_matrix_china = c(0.810810810810811, 0.0908559523547268, 0.372736406439194, 1.27360250772, 0.200569529052988, 0.375083342749019, 0.60252680195839, 0.0934189610338407, 0.0225225225225225, 0.0904095466183592, 2.4392523364486, 0.140093983348316, 0.706545801082683, 0.942937990573664, 0.27920963239528, 0.326366336169345, 0.196893495540358, 0.106045179398683, 0.289504965045504, 0.109348487445688, 1.76086956521739, 0.923069180041088, 0.93772012267962, 0.965186137047983, 0.274120168579709, 0.116564256844925, 0.0773400190233669, 0.91820215964926, 0.511898453884688, 0.85680985412458, 2.70542635658915, 1.41323192857305, 0.993399938008648, 0.719603621821669, 0.146103509716984, 0.07633130138862, 0.13119227828341, 0.619819944222649, 0.789700390093264, 1.28218991206025, 2.17699115044248, 1.1199461877854, 0.514253349451317, 0.496466649026704, 0.101504389707241, 0.259078294801222, 0.193808465356441, 0.858341528544101, 0.951750199084178, 1.18265232149625, 2.31730769230769, 0.977037933291252, 0.606164987575222, 0.4393566902894, 0.552747314447092, 0.300880970126328, 0.323770338070664, 0.915670466885606, 0.721247101248993, 1.29765260904839, 2.76146788990826, 0.959867553314515, 0.340125585278128, 0.121809161946423, 0.25799743320884, 0.1956843612527, 0.264241585561661, 0.989672909331423, 1.14428055461657, 1.36428769674242, 1.96363636363636, 1.0266513139522, 0.0447824174066075, 0.211894911958445, 0.197988778289041, 0.210517772531686, 0.308554588199316, 1.26474943927563, 0.737190168823191, 1.56555579008225, 2.0625)
+contact_matrix_china = c(0.810810810810811, 0.0908559523547268, 0.372736406439194,
+                         1.27360250772, 0.200569529052988, 0.375083342749019,
+                         0.60252680195839, 0.0934189610338407, 0.0225225225225225,
+                         0.0904095466183592, 2.4392523364486, 0.140093983348316,
+                         0.706545801082683, 0.942937990573664, 0.27920963239528,
+                         0.326366336169345, 0.196893495540358, 0.106045179398683,
+                         0.289504965045504, 0.109348487445688, 1.76086956521739,
+                         0.923069180041088, 0.93772012267962, 0.965186137047983,
+                         0.274120168579709, 0.116564256844925, 0.0773400190233669,
+                         0.91820215964926, 0.511898453884688, 0.85680985412458,
+                         2.70542635658915, 1.41323192857305, 0.993399938008648,
+                         0.719603621821669, 0.146103509716984, 0.07633130138862,
+                         0.13119227828341, 0.619819944222649, 0.789700390093264,
+                         1.28218991206025, 2.17699115044248, 1.1199461877854,
+                         0.514253349451317, 0.496466649026704, 0.101504389707241,
+                         0.259078294801222, 0.193808465356441, 0.858341528544101,
+                         0.951750199084178, 1.18265232149625, 2.31730769230769,
+                         0.977037933291252, 0.606164987575222, 0.4393566902894,
+                         0.552747314447092, 0.300880970126328, 0.323770338070664,
+                         0.915670466885606, 0.721247101248993, 1.29765260904839,
+                         2.76146788990826, 0.959867553314515, 0.340125585278128,
+                         0.121809161946423, 0.25799743320884, 0.1956843612527,
+                         0.264241585561661, 0.989672909331423, 1.14428055461657,
+                         1.36428769674242, 1.96363636363636, 1.0266513139522,
+                         0.0447824174066075, 0.211894911958445, 0.197988778289041,
+                         0.210517772531686, 0.308554588199316, 1.26474943927563,
+                         0.737190168823191, 1.56555579008225, 2.0625)
 
 data.frame(contacts=contact_matrix_china) %>%
   tbl_df() %>%
@@ -126,20 +152,28 @@ data_list_model16A = list(
 
 # test
 M_model16 = stan_model("models/model16.stan")
-T_model16 = sampling(M_model16,data = data_list_model16A,iter = 5,chains = 1,init=0.5,control=list(max_treedepth=10,adapt_delta=0.8))
+T_model16 = sampling(M_model16,data = data_list_model16A,iter = 5,chains = 1,
+                     init=0.5,control=list(max_treedepth=10,adapt_delta=0.8))
 # print(T_model16,pars=c("beta","eta","epsilon","rho","pi"))
 
 
 # Create data and bash files ----
-bashfile_rdump("model16",id="A",data_list_model16A,warmup=500,iter=500,adapt_delta=0.8,max_depth=10,init=0.5,timelimit=12,chains=4)
+bashfile_rdump("model16",id="A",data_list_model16A,warmup=500,iter=500,
+               adapt_delta=0.8,max_depth=10,init=0.5,timelimit=12,chains=4)
 
 # Copy on cluster
-# system("scp /home/julien/Dropbox/Unibe/covid-19/covid_adjusted_cfr/models/model16.stan /home/julien/Dropbox/Unibe/covid-19/covid_adjusted_cfr/run_models/sb_model16* /home/julien/Dropbox/Unibe/covid-19/covid_adjusted_cfr/run_models/data_SIM_model16A* /home/julien/Dropbox/Unibe/covid-19/covid_adjusted_cfr/run_models/data_S_model16* UBELIX:projects/COVID_age/model/.")
+ # system("scp /home/julien/Dropbox/Unibe/covid-19/covid_adjusted_cfr/models/model16.stan
+ # /home/julien/Dropbox/Unibe/covid-19/covid_adjusted_cfr/run_models/sb_model16*
+ # /home/julien/Dropbox/Unibe/covid-19/covid_adjusted_cfr/run_models/data_SIM_model16A*
+ # /home/julien/Dropbox/Unibe/covid-19/covid_adjusted_cfr/run_models/data_S_model16*
+ # UBELIX:projects/COVID_age/model/.")
 # module load Boost/1.66.0-foss-2018a
 
 # Copy back posterior samples
-system("scp UBELIX:projects/COVID_age/model/S_model16A_2020-05-04-11-01-46* /home/julien/Dropbox/Unibe/covid-19/covid_adjusted_cfr/posterior_samples/.")
-system("scp UBELIX:projects/COVID_age/model/data_S_model16A_2020-05-04-11-01-46.R /home/julien/Dropbox/Unibe/covid-19/covid_adjusted_cfr/posterior_samples/.")
+system("scp UBELIX:projects/COVID_age/model/S_model16A_2020-05-04-11-01-46*
+       /home/julien/Dropbox/Unibe/covid-19/covid_adjusted_cfr/posterior_samples/.")
+system("scp UBELIX:projects/COVID_age/model/data_S_model16A_2020-05-04-11-01-46.R
+       /home/julien/Dropbox/Unibe/covid-19/covid_adjusted_cfr/posterior_samples/.")
 
 # Load posterior samples 
 D_S_model16A = read_rdump("posterior_samples/data_S_model16A_2020-05-04-11-01-46.R")
